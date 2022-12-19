97b224ca783befa6b3a93ead06a4f46120d37bd8
DenisIshuga <denisishuga@gmail.com>
2022-12-19 09:28:49 +0300

    workflow changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,15 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 
 
 void main() {
-  
+  test('String.split() splits the string on the delimiter', () {
+    var string = 'foo,bar,baz';
+    expect(string.split(','), equals(['foo', 'bar', 'baz']));
+  });
+
+  test('String.trim() removes surrounding whitespace', () {
+    var string = '  foo ';
+    expect(string.trim(), equals('foo'));
+  });
 }
